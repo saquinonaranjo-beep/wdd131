3b8fe8ffadfbb27880b9d02132c8a4df45cc4ec5
seraquino <saquinonaranjo@byupathway.edu>
2026-03-11 20:41:34 -0600

    nice

--- week02/scripts/scripts.js
+++ week02/scripts/scripts.js
@@ -2,22 +2,9 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
-button.addEventListener('click', function () {
-
-  const li = document.createElement('li');
-  const deleteButton = document.createElement('button');
-
-  li.textContent = input.value;
-  deleteButton.textContent = '❌';
-
-  li.append(deleteButton);
-  list.append(li);
-
-  input.value = '';
-  input.focus();
-
-  deleteButton.addEventListener('click', function () {
-    list.removeChild(li);
-  });
-
-});
+const li = document.createElement("li");
+const deleteButton = document.createElement('button');
+li.textContent = input.value;
+deleteButton.textContent = 'Delete';
+li.append(deleteButton);
+list.append(li);
