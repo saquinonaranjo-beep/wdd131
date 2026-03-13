f059170bb5f0c06d887bf09cc662c65fdfaae674
seraquino <saquinonaranjo@byupathway.edu>
2026-03-12 21:07:17 -0600

    finished

---
+++ week02/scripts/bom.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener("click", function() {   
+    const li = document.createElement("li");
+    const deleteButton = document.createElement('button');
+   
+
+    li.textContent = input.value;
+    
+    deleteButton.textContent = '❌';
+    deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+
+    deleteButton.addEventListener('click', function() {
+        list.removeChild(li);
+    });
+    
+    li.append(deleteButton);
+    list.append(li);
+
+    input.value = '';
+    input.focus();
+})
