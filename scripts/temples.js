ea5b8e6f20096443fc11ce154860e2426dbf0d20
seraquino <saquinonaranjo@byupathway.edu>
2026-03-15 00:08:47 -0600

    nice

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,16 +2,20 @@ const menuButton = document.getElementById("menuButton");
 const navMenu = document.getElementById("navMenu");
 
 menuButton.addEventListener("click", () => {
-    navMenu.classList.toggle("show");
 
-    if (navMenu.classList.contains("show")) {
-        menuButton.textContent = "✖";
-    } else {
-        menuButton.textContent = "☰";
-    }
+navMenu.classList.toggle("show");
+
+if(navMenu.classList.contains("show")){
+menuButton.textContent = "✖";
+}
+else{
+menuButton.textContent = "☰";
+}
+
 });
 
-document.querySelector("#currentYear").textContent = new Date().getFullYear();
+document.querySelector("#currentYear").textContent =
+new Date().getFullYear();
 
 document.querySelector("#lastModified").textContent =
 `Last Modified: ${document.lastModified}`;
